991da746051e91648379f4f901686c1a393d05dc
David Esposito <david.esposito@bettercloud.com>
2017-01-20 08:33:54 -0500

    Initial commit
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

---
+++ src/main/java/com/bettercloud/challenges/ProgrammingChallengesApplication.java
@@ -0,0 +1,12 @@
+package com.bettercloud.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingChallengesApplication.class, args);
+	}
+}
