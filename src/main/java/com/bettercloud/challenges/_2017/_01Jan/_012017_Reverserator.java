a2db5bd22e5170287fa352d6b993b68ad79e93e3
Madhu Sigdel <madhu.sigdel@bettercloud.com>
2017-01-20 20:46:23 -0500

    Reversetator stream solution

--- src/main/java/com/bettercloud/challenges/_2017/_01Jan/_012017_Reverserator.java
+++ src/main/java/com/bettercloud/challenges/_2017/_01Jan/_012017_Reverserator.java
@@ -1,5 +1,10 @@
 package com.bettercloud.challenges._2017._01Jan;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.pow;
+
 /**
  * Created by davidesposito on 1/20/17.
  */
@@ -25,7 +30,20 @@ public class _012017_Reverserator {
 
         @Override
         public int process(int num) {
-            return 0;
+            /** Solution 1
+             return Integer.valueOf(StreamSupport.stream(Spliterators.spliteratorUnknownSize(Integer.toString(num)
+             .chars()
+             .map(c->c-'0')
+             .boxed()
+             .map(Object::toString)
+             .collect(Collectors.toCollection(LinkedList::new))
+             .descendingIterator(), Spliterator.ORDERED), false).collect(Collectors.joining()));
+             */
+            /** Solution 2 */
+            return Integer.valueOf(IntStream.range(0, String.valueOf(num).length()).map(i -> (int)((num % pow(10, i + 1) - num % pow(10, i)) / pow(10, i)))
+                    .boxed()
+                    .map((Object::toString))
+                    .collect(Collectors.joining()));
         }
     }
 }
